b4e563b24507403f32bcd5c591b4e9ab476883e1
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-07-26 19:51:27 -0400

    IMAGENES

--- PRACTICE/Users.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-url =  "https://randomuser.me/api/?page=3&results=100"
-
-#get the data
-response = requests.get(url)
-data = response.json()
-print(json.dumps(data, indent=4))
