f8654ecc0e6074967bed4854430f422e086e6773
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-08-09 19:49:24 -0400

    cambios menores

--- CORE/dojo_survey/server.py
+++ CORE/dojo_survey/server.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect
+from flask import request
 
 app = Flask(__name__)
+app.secret_key = "clave"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-if '__name__' == '__main__':
+@app.route('/process',methods=['POST'])
+def process():
+    print(request.form)
+    return redirect('/result')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+
+
+
+
+
+if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
