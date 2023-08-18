bd909cc622f37390b855d172e606b1dcca9564b0
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-08-18 12:19:06 -0400

    modularizado

---
+++ CORE/user_crud_mvc/flask_app/controllers/user_redirects.py
@@ -0,0 +1,73 @@
+# burgers.py
+from flask_app import app
+from flask import render_template,redirect,request,session,flash
+from flask_app.models.users import User
+
+
+
+@app.route('/')
+def index():
+    users = User.get_all()
+    print(users)
+    return render_template("users.html",users=users)
+
+
+@app.route('/user/<int:id>/')
+def user(id):
+    data = {
+        "id":id
+    }
+    user = User.get_one(data)
+    return render_template("user.html",user=user)
+
+
+@app.route('/new_user/')
+def new_user():
+    return render_template("new_user.html")
+
+
+@app.route('/user/edit/<int:id>/')
+def edit_user(id):
+    data = {
+        "id":id
+    }
+    user = User.get_one(data)
+    return render_template("edit.html",user=user)
+
+
+@app.route('/add_user',methods=['POST'])
+def add_user():
+    print(request.form)
+    data = {
+        "fname" : request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+
+    User.save(data)
+
+    return redirect('/')
+
+
+@app.route('/update_user/<int:id>',methods=['POST'])
+def update_user(id):
+    id_usuario = id
+    data = {
+        "id":id,
+        "fname" : request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+
+    User.update(data)
+
+    return redirect('/user/'+str(id_usuario)+'/')
+
+
+@app.route('/delete_user/<int:id>/')
+def delete_user(id):
+    data = {
+        "id":id
+    }
+    User.delete(data)
+    return redirect('/')
---
+++ CORE/user_crud_mvc/server.py
@@ -0,0 +1,8 @@
+from flask_app.controllers.user_redirects import *
+from flask_app import app
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=5500)
